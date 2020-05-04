88a245f4e6ffa15d3d5eaf99bd0be57640bb6914
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 20:27:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  new_array = []
+    i = 0 
+  while i < array.length do
+    
+    new_array.push(Math.sqrt(array[i])
+  end
+  i += 1  
+  new_array
 end
